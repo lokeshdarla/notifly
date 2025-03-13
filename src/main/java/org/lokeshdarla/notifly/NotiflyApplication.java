4a00007fe8efbd8f58a8d6ebb34f6469d6ea3a56
lokeshdarla <lokeshnagasaidarla@gmail.com>
2025-03-13 22:37:01 +0530

    init

---
+++ src/main/java/org/lokeshdarla/notifly/NotiflyApplication.java
@@ -0,0 +1,13 @@
+package org.lokeshdarla.notifly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotiflyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotiflyApplication.class, args);
+    }
+
+}
